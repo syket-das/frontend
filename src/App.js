ebda19931b35cb91e40a7d34ded5b1e9edddff48
syket-das <saiketsd23@gmail.com>
2022-10-26 16:15:20 +0530

    upload to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useRef, useState } from 'react';
+import { FileUpload } from 'primereact/fileupload';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [file, setFile] = useState('');
+
+  const onUpload = (event) => {
+    setFile(event.files[0]);
+  };
+
+  useEffect(() => {
+    console.log(file);
+    if (file) {
+    const asyncUpload = async () => {
+      const formData = new FormData();
+      formData.append('file', file);
+      const res = await axios.post('{backend api url}', formData);
+      console.log(res);
+    };
+    asyncUpload();
+  }
+  }, [file]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        marginTop: '30%',
+      }}
+    >
+      <div>
+        <div className="card">
+          <FileUpload
+            name="demo"
+            customUpload
+            uploadHandler={onUpload}
+            accept="image/*"
+          />
+        </div>
+      </div>
     </div>
   );
 }
