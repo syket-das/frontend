0883e033cab346f6840081fdffb574fb8f6fd68a
syket-das <saiketsd23@gmail.com>
2022-10-30 10:38:21 +0530

    s

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import React, { useRef, useState } from 'react';
 import { FileUpload } from 'primereact/fileupload';
 import { useEffect } from 'react';
 import axios from 'axios';
+import { Form, Button } from 'react-bootstrap';
 
 function App() {
   const [file, setFile] = useState('');
+
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
 
   const onUpload = (event) => {
     setFile(event.files[0]);
@@ -15,21 +19,27 @@ function App() {
   useEffect(() => {
     console.log(file);
     if (file) {
-    const asyncUpload = async () => {
-      const formData = new FormData();
-      formData.append('file', file);
-      const res = await axios.post('{backend api url}', formData);
-      console.log(res);
-    };
-    asyncUpload();
-  }
+      const asyncUpload = async () => {
+        const formData = new FormData();
+        formData.append('file', file);
+        const res = await axios.post('{backend api url}', formData);
+        console.log(res);
+      };
+      asyncUpload();
+    }
   }, [file]);
+
+
+  const formSubmit =  (e) => {
+    e.preventDefault();
+    console.log(name, age);
+  }
 
   return (
     <div
-      className="App"
+      className=""
       style={{
-        marginTop: '30%',
+        marginTop: '10%',
       }}
     >
       <div>
@@ -41,6 +51,41 @@ function App() {
             accept="image/*"
           />
         </div>
+      </div>
+      <div
+        style={{
+          marginTop: '10%',
+          width: '350px',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <Form onSubmit={formSubmit}>
+          <Form.Group className="mb-3" controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="age">
+            <Form.Label>Age</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Enter age"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+              
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css";                                //icons
